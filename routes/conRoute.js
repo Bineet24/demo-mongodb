809a576338351ed9fe9c21f04cb583dc2968602c
Bineet Sharma <86036476+Bineet24@users.noreply.github.com>
2023-03-17 12:21:32 +0530

    Update conRoute.js

--- routes/conRoute.js
+++ routes/conRoute.js
@@ -4,8 +4,8 @@ const conController = require('./../controllers/conController');
 
 const router = express.Router();
 
-router.route('/').get(conController.getAllCons).post(conController.checkBody,conController.createCon).delete(conController.deleteCon);
-
+router.route('/').get(conController.getAllCons).post(conController.createCon).delete(conController.deleteCon);
+//conController.checkBody,
 router.route('/:id').get(conController.getCon).patch(conController.updateCon);
 //.delete(conController.deleteCon)
 
